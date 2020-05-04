554163a08c7ae423cecef95e5516a6f0b5886cda
thliao <l0404th@gmail.com>
2020-05-04 22:17:36 +0800

    add a module for drawing tanglegram.py

--- vis_for/newick/draw_tanglegram.py
+++ vis_for/newick/draw_tanglegram.py
@@ -7,6 +7,7 @@ Further, it also implement some useful transferring function from iTOL.
 
 from collections import defaultdict
 
+import click
 import plotly
 import plotly.graph_objs as go
 from tqdm import tqdm
@@ -22,7 +23,7 @@ def get_preferred_scale(newick1, newick2):
     num_2 = len(t2.get_leaf_names())
 
     yscale = num_1 / num_2
-    return 1/yscale
+    return 1 / yscale
 
 
 def parse_color_scheme_files(file, extra_set=False):
@@ -84,7 +85,7 @@ def main(newick1, newick2,
     r2color.update(parse_color_scheme_files(color_file2, extra_set=extra_set))
     # add color into generated trace
     tqdm.write('adding color')
-    name2trace = {_['name']:_ for _ in datas if _['name'] is not None}
+    name2trace = {_['name']: _ for _ in datas if _['name'] is not None}
     for l, color in l2color.items():
         if color != '#00000' and l in labels_draw_text:
             trace = name2trace[l]
@@ -98,9 +99,9 @@ def main(newick1, newick2,
     # add dendrogram parts into figure.
     # data1/newick1 would be the left, data2/newick2 would be the right and the leaves of it will point to left
     # for _ in datas:
-    fig.add_traces(datas, rows=[1]*len(datas), cols=[1]*len(datas))
+    fig.add_traces(datas, rows=[1] * len(datas), cols=[1] * len(datas))
     # for _ in datas2:
-    fig.add_traces(datas2, rows=[1]*len(datas2), cols=[3]*len(datas2))
+    fig.add_traces(datas2, rows=[1] * len(datas2), cols=[3] * len(datas2))
 
     # init data of middle part
     # get the y-coordinate information from below. put them into two dict.
@@ -115,18 +116,18 @@ def main(newick1, newick2,
         l2r[leaf1] = leaf2s
 
     # init the data from above mapping dict
-    c2data = defaultdict(lambda : ([],[]))
+    c2data = defaultdict(lambda: ([], []))
     for l, color in l2color.items():
         _xs = c2data[color][0]
         _ys = c2data[color][1]
         rs = l2r.get(l, [])
-        l_y = left_data.get(l,0)
+        l_y = left_data.get(l, 0)
         for r in rs:
             r_y = right_data[r]
             _xs += [0, 1, None]
             _ys += [l_y, r_y, None]
 
-    for color,(_xs,_ys) in c2data.items():
+    for color, (_xs, _ys) in c2data.items():
         trace = go.Scatter(x=_xs,
                            y=_ys,
                            mode='lines',
@@ -146,17 +147,37 @@ def main(newick1, newick2,
     return fig
 
 
-if __name__ == '__main__':
-    newick1 = './nxrA.newick'
-    newick2 = './species.txt'
+@click.command()
+@click.option("-newick1",help="first tree file, would be placed on the left")
+@click.option("-newick2",help="second tree file, would be placed on the right")
+@click.option("-cf1",help="the file for color annotation to first tree and the links")
+@click.option("-cf2",help="the file for color annotation to second tree ")
+@click.option("-length", default="max",help="the length of leaves you want to extend to. normally all leaves will extend to identical length which similar to the longest one")
+@click.option("-sep", default="_",help="the separator which used to . ")
+@click.option("-extra_set", "extra_set", is_flag=True, default=False)
+def cli(newick1, newick2, cf1, cf2, length, sep, extra_set):
+    main(newick1=newick1,
+         newick2=newick2,
+         color_file1=cf1,
+         color_file2=cf2,
+         l_legnth=length,
+         sep=sep,
+         extra_set=extra_set)
 
-    color_file1 = './gene_annotation.txt'
-    color_file2 = './phylum_annotate.txt'
+
+if __name__ == '__main__':
+    from os.path import dirname, join
+
+    example_dir = join(dirname(dirname(dirname(__file__))), 'example', 'tanglegram')
+
+    newick1 = join(example_dir, 'nxrA.newick')
+    newick2 = join(example_dir, 'species.txt')
+
+    color_file1 = join(example_dir, 'gene_annotation.txt')
+    color_file2 = join(example_dir, 'phylum_annotate.txt')
     fig = main(newick1, newick2,
                color_file1, color_file2,
                l_legnth='max', sep='_', extra_set='rename')
-
     fig.layout.width = 1400
     fig.layout.height = 3000
-
-    fig.write_html('./p.html')
+    fig.write_html(join(example_dir, 'p.html'))
